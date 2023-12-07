8ef80019cd6654f88bd584653a212b22a83aa2bc
phucnhan <phucnhan0406@gmail.com>
2023-12-08 00:47:19 +0700

    1
    1

---
+++ src/server.js
@@ -0,0 +1,66 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = 5000;
+
+// Create connection to the database
+const db = mysql.createConnection({
+    host: 'db4free.net', // Replace with your database host
+    user: 'phucnhan0406', // Replace with your database username
+    password: 'phucnhan0406@', // Replace with your database password
+    database: 'sales_clock',
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Register route for user registration
+app.post('/api/register', (req, res) => {
+    const { name, email, password } = req.body;
+
+    const sql = 'INSERT INTO Users (name, email, password) VALUES (?, ?, ?)';
+    db.query(sql, [name, email, password], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error registering user');
+            return;
+        }
+        res.status(201).send('User registered successfully');
+    });
+});
+
+// Route for user login
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = 'SELECT * FROM Users WHERE email = ? AND password = ?';
+    db.query(sql, [email, password], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error logging in');
+            return;
+        }
+
+        if (result.length > 0) {
+            res.status(200).send('Login successful');
+        } else {
+            res.status(401).send('Invalid credentials');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
