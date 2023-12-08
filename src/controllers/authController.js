bbe9028320b6f75d790990cb7df9963a646c1cd0
phucnhan <phucnhan0406@gmail.com>
2023-12-08 17:05:52 +0700

    register

---
+++ src/controllers/authController.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const { createUser, findUserByEmail } = require(path.join(__dirname, '..', 'api'));
+
+// Rest of your code...
+
+
+
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ success: false, message: 'Name, email, and password are required.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await createUser(name, email, hashedPassword);
+
+        res.json({ success: true, message: 'User registered successfully.' });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ success: false, message: 'Internal server error during registration.' });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await findUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'Invalid credentials.' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ success: false, message: 'Invalid credentials.' });
+        }
+
+        // JWT signing with the secret from the .env file
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ success: true, token });
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ success: false, message: 'Internal server error during login.' });
+    }
+};
+
+module.exports = { register, login };
